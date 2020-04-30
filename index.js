afc0b158e26ebf3cfa68e27f70ac546963ea532d
Davi <davicamiloribeiro@gmail.com>
2020-04-30 08:41:12 -0300

    Alteração do template do arquivo PDF enviado por e-mail

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ app.use(session({
 
 //Arquivos estaticos
 app.use(express.static('public'));
-app.use('/public', express.static('public'))
+app.use('/public', express.static('public'));
+app.use('/public/img/', express.static('./public/img'));
 
 //Body parser
 app.use(bodyParser.urlencoded({extended: false}));
