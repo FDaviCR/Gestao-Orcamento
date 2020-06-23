59918b71f982764361f83325173ada7fb575bbc3
Davi <davicamiloribeiro@gmail.com>
2020-06-22 23:42:50 -0300

    atualização Phantom e BD

--- database/database.js
+++ database/database.js
@@ -1,12 +1,20 @@
 const Sequelize = require('sequelize');
 
 const connection = new Sequelize(
+    'aptumgst','root1994','password',
+    {
+        host: 'mysql669.umbler.com',
+        dialect: 'mysql',
+        timezone: "-03:00"
+    }
+    /*
     'aptumgst','root','password',
     {
         host: 'localhost',
         dialect: 'mysql',
         timezone: "-03:00"
     }
+    */
 );
 
 module.exports = connection;
